93f3c06ef44f4fa9a3ff26f9f6ffe1ea39f1266e
eg7eg7 <eg7eg7@gmail.com>
2019-08-15 21:53:47 +0300

    blabla

---
+++ scriptUploadToElastic/download_files_from_net.py
@@ -0,0 +1,49 @@
+
+import urllib.request as url_request
+import urllib2 as url_retrieve
+
+
+def readURL():
+    url = "http://archive.moia.gov.il/Publications/talmid_he.pdf"
+
+    file_name = url.split('/')[-1]
+    u = url_request.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print("Downloading: %s Bytes: %s" % (file_name, file_size))
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print(status)
+
+    f.close()
+
+
+def is_downloadable(url):
+    """
+    Does the url contain a downloadable resource
+    """
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    path = 'C:\\2.pdf'
+    #readPDF(path)
+    readURL()
---
+++ scriptUploadToElastic/uploadscript.py
@@ -0,0 +1,49 @@
+
+import slate3k as slate
+import json
+from datetime import datetime
+#from elasticsearch import Elasticsearch
+#from elasticsearch import helpers
+
+def readPDF(filename):
+    with open(filename, 'rb') as f:
+        extracted_text = slate.PDF(f)
+    text = extracted_text.text(False)
+    text = text.split("\n\n")
+    json_list = []
+    for p in text:
+        if filter_text(p):
+            reversed_p = p[::-1]
+            #print(reversed_p)
+            json_list.append(create_json(filename, reversed_p, "Dummy_url", datetime.now()))
+    return json_list
+
+
+def filter_text(text):
+    min_length = 40
+    if len(text) > min_length:
+        return True
+    return False
+
+
+def create_json(filename, paragraph, url, timestamp):
+    my_map = {
+        "Filename": filename,
+        "Paragraph": paragraph,
+        "URL": url,
+        "timestamp": timestamp.__str__(),
+        "_index": "olim",
+        "_type": "doc"
+    }
+
+    json_text = json.dumps(my_map)
+    return json_text
+
+
+if __name__ == '__main__':
+    path = 'C:\\2.pdf'
+#    es = Elasticsearch()
+    json_list = readPDF(path)
+    print(json_list)
+    print(len(json_list))
+    #helpers.bulk(es, json_list)
